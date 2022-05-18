bd5ffab5e007f783fb6d0945d6830b85e2283f87
tony <806003097@qq.com>
2022-05-19 00:08:40 +0800

    进行基础样式的搭建

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-//开发的时候引入该tsx，用于显示样式，发布的时候，组件不会和样式一起发布
+// !开发的时候引入该tsx，用于显示样式，发布的时候，组件不会和样式一起发布
+// ? 如何解决？
 import './style/style.less';
 import classNames from 'classnames';
 
@@ -13,7 +14,7 @@ const Button: React.FC<ButtonProps> = (props) => {
   const classes = classNames('tdesign-btn', {
     'block-btn': block,
   });
-  return <div className={classes}>Button</div>;
+  return <button className={classes}>Button</button>;
 };
 
 export default Button;
