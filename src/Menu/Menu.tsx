83a2684aa887cd11d032383d58a852e1d0a04656
gwjlshebei <806003097@qq.com>
2022-06-25 11:19:50 +0800

    stash menu component

--- src/Menu/Menu.tsx
+++ src/Menu/Menu.tsx
@@ -1,15 +1,20 @@
 import React, { ReactNode } from 'react';
-import type { MenuProps as RcMenuProps, MenuRef } from 'rc-menu';
+// import type { MenuProps as RcMenuProps, MenuRef } from 'rc-menu';
 import RcMenu, { ItemGroup } from 'rc-menu';
+import type { MenuRef, SelectEventHandler } from './interface';
 interface MenuProps {
+  style?: React.CSSProperties;
+  className?: string;
   defaultIndex?: number;
   children?: ReactNode;
+  onSelect?: SelectEventHandler;
+  mode?: MenuMode;
 }
 
 export type MenuMode = 'vertical' | 'horizontal';
 
-const Menu: React.FC<MenuProps> = (props) => {
+const Menu = React.forwardRef<MenuRef, MenuProps>((props, ref) => {
   return <ul>{props.children}</ul>;
-};
+});
 
 export default Menu;
